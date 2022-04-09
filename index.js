187b1cf1ece2122bff4a3683d8d7e71bd811aa91
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-04-09 13:17:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Comentário em JavaScript em uma linha
+
+/*
+Comentário em várias 
+linhas em bloco
+*/
+
+// atalho CTRL + : OU CTRL + /
+
+/* Atalho ALT + SHIFT + a */
+
+var numero = 10
+numero = 5
+
+//ferramenta de depuração 
+// console.log(numero) // Como se fosse o print
+
+/* 
+TIPOS DE VARIAVEIS
+String -> é um texto, ultizada as aspas para delimitar o seu conteudo (simples ou duplas)
+*/
+
+var nome = "Larissa Ramos"
+console.log(typeof(nome)) // metodo Typeof retorna uma string com o tipo de variavel
